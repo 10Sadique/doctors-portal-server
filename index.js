8b2ce3014d6624a47b1b5081487cb788597bfc60
10Sadique <jafarsjahan@gmail.com>
2022-11-16 18:22:13 +0600

    appointment options loading endpoint added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,39 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// MongoDB setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+// mongdb data fetching
+async function run() {
+    try {
+        const database = client.db('doctorsPortal');
+
+        // appointment options collection
+        const appointmentOptionsCollection =
+            database.collection('appointmentOptions');
+
+        // get appointment options
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionsCollection
+                .find(query)
+                .toArray();
+
+            res.send(options);
+        });
+    } finally {
+    }
+}
+
+run().catch((err) => console.log(err));
 
 // root ('/') endpoint
 app.get('/', (req, res) => {
